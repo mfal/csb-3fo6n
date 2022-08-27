17a7136263db26233336bef647261e5d71bcfa3a
Code Sandbox <hello@codesandbox.io>
2022-08-27 15:43:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6c095c1af6dd9856c262101902f48d5e.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fdfa03658b25c3bbc02",
+    "url": "https://mfal.github.io/csb-3fo6n/static/css/main.714648d7.css"
+  },
+  {
+    "revision": "3fdfa03658b25c3bbc02",
+    "url": "https://mfal.github.io/csb-3fo6n/static/js/main.3fdfa036.js"
+  },
+  {
+    "revision": "ac96e2735215b898c59441478928f1a3",
+    "url": "https://mfal.github.io/csb-3fo6n/index.html"
+  }
+];
